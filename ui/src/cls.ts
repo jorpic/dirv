2c2f5ab3237c9f3319993f1143a81b40f37557a8
Max Taldykin <jorpic@gmail.com>
2024-11-30 15:11:03 +0400

    ui: support booleans in cls()

--- ui/src/cls.ts
+++ ui/src/cls.ts
@@ -1,6 +1,13 @@
 export function cls() {
   const res = [];
   for(let arg of arguments) {
+    if(typeof arg === "boolean") {
+      if(arg) {
+        continue;
+      } else {
+        break;
+      }
+    }
     if(typeof arg === "object") {
       for(let [k, v] of Object.entries(arg)) {
         if(v) {
